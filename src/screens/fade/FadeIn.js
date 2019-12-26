bbb6ef6213760e8357538f87621b232d140f074e
Rashid <rashid@nextgbl.com>
2019-12-26 13:26:48 +0530

    fade animations

--- src/screens/fade/FadeIn.js
+++ src/screens/fade/FadeIn.js
@@ -1,17 +1,42 @@
 import React, {Component} from 'react';
-import {View, Animated, Text} from 'react-native';
+import {View, Animated, StyleSheet} from 'react-native';
 
 export default class FadeIn extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      startValue: new Animated.Value(0),
+      endValue: 1,
+      duration: 8000,
+    };
+  }
+
+  componentDidMount() {
+    Animated.timing(this.state.startValue, {
+      toValue: this.state.endValue,
+      duration: this.state.duration,
+    }).start();
   }
 
   render() {
     return (
-      <View>
-        <Text> FadeIn </Text>
+      <View style={styles.container}>
+        <Animated.View
+          style={[styles.square, {opacity: this.state.startValue}]}
+        />
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  square: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'green',
+  },
+});
--- src/screens/fade/FadeOut.js
+++ src/screens/fade/FadeOut.js
@@ -1,17 +1,42 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Animated, StyleSheet} from 'react-native';
 
-export default class FadeOut extends Component {
+export default class FadeIn extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      startValue: new Animated.Value(1),
+      endValue: 0,
+      duration: 8000,
+    };
+  }
+
+  componentDidMount() {
+    Animated.timing(this.state.startValue, {
+      toValue: this.state.endValue,
+      duration: this.state.duration,
+    }).start();
   }
 
   render() {
     return (
-      <View>
-        <Text> FadeOut </Text>
+      <View style={styles.container}>
+        <Animated.View
+          style={[styles.square, {opacity: this.state.startValue}]}
+        />
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  square: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'red',
+  },
+});
